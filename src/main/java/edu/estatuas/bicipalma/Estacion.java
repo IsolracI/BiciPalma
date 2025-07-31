79deb43d68ea6227ed7c818f024f0bceb16f55b4
IsolracI <gabrieldiacich@gmail.com>
2025-07-31 13:04:13 +0200

    build(main): crear clase Estacion

---
+++ src/main/java/edu/estatuas/bicipalma/Estacion.java
@@ -0,0 +1,15 @@
+package edu.estatuas.bicipalma;
+
+public class Estacion {
+    private int id;
+    private String direccion;
+    private int numAnclajes;
+
+    Estacion(int id, String direccion, int numAnclajes) {
+        this.id = id;
+        this.direccion = direccion;
+        this.numAnclajes = numAnclajes;
+    }
+
+    
+}
