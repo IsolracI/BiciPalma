24a11582d35eb7bae4195765cf315b49b6f80290
|solrac| <gabrieldiacich@gmail.com>
2026-03-31 01:53:40 +0200

    feat(Bicicleta): diseñar clase Anclaje

---
+++ src/main/java/edu/estatuas/bicipalma/domain/estacion/Anclaje.java
@@ -0,0 +1,33 @@
+package edu.estatuas.bicipalma.domain.estacion;
+
+import edu.estatuas.bicipalma.domain.bicicleta.Movil;
+
+public class Anclaje {
+    private boolean ocupado;
+    private Movil bici;
+
+    Anclaje() {}
+
+    boolean isOcupado() {
+        return this.ocupado;
+    }
+
+    Movil getBici() {
+        return this.bici;
+    }
+
+    void anclarBici(Movil bici) {
+        this.bici = bici;
+        this.ocupado = true;
+    }
+
+    void liberarBici() {
+        this.bici = null;
+        this.ocupado = false;
+    }
+
+    @Override
+    public String toString() {
+        return "anclaje libre: " + isOcupado();
+    }
+}
