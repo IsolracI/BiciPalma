09bd072f7ce34f2f1705a0022e891b9e6f121cd4
|solrac| <gabrieldiacich@gmail.com>
2026-03-31 02:18:14 +0200

    feat(Estacion): diseñar clase Estacion hasta primera historia de usuario

---
+++ src/main/java/edu/estatuas/bicipalma/domain/estacion/Estacion.java
@@ -0,0 +1,13 @@
+package edu.estatuas.bicipalma.domain.estacion;
+
+public class Estacion {
+    private final int id;
+    private final String direccion;
+    private final Anclajes anclajes;
+
+    public Estacion(int id, String direccion, int numAnclajes) {
+        this.id = id;
+        this.direccion = direccion;
+        anclajes = new Anclajes(numAnclajes);
+    }
+}
